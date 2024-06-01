0196392fa4a18891a12e053d71cc1239645cf6e5
mochi22 <jdhdbemd@gmail.com>
2024-06-01 20:25:59 +0900

    add error handle

--- gacha/gacha.go
+++ gacha/gacha.go
@@ -11,21 +11,57 @@ func init() {
 	rand.Seed(time.Now().Unix())
 }
 
-func DrawN(p *Player, n int) ([]*Card, map[Rarity]int) {
-	p.draw(n)
-
-	results := make([]*Card, n)
-	summary := make(map[Rarity]int)
-	for i := 0; i < n; i++ {
-		results[i] = draw()
-		summary[results[i].Rarity]++
-	}
-
-	// 変数resultsとsummaryの値を戻り値として返す
-	return results, summary
+type Play struct {
+	player  *Player
+	results []*Card
+	summary map[Rarity]int
+	err     error
 }
 
-func draw() *Card {
+func NewPlay(p *Player) *Play {
+	return &Play{
+		player:  p,
+		summary: make(map[Rarity]int),
+	}
+}
+
+func (p *Play) Results() []*Card {
+	return p.results
+}
+
+func (p *Play) Result() *Card {
+	if len(p.results) == 0 {
+		return nil
+	}
+	return p.results[len(p.results)-1]
+}
+
+func (p *Play) Summary() map[Rarity]int {
+	return p.summary
+}
+
+func (p *Play) Err() error {
+	return p.err
+}
+
+func (p *Play) Draw() bool {
+	if p.err != nil {
+		return false
+	}
+
+	if err := p.player.draw(1); err != nil {
+		p.err = err
+		return false
+	}
+
+	card := p.draw()
+	p.results = append(p.results, card)
+	p.summary[card.Rarity]++
+
+	return p.player.DrawableNum() > 0
+}
+
+func (p *Play) draw() *Card {
 	num := rand.Intn(100)
 
 	switch {
@@ -34,9 +70,7 @@ func draw() *Card {
 	case num < 95:
 		return &Card{Rarity: RarityR, Name: "オーク"}
 	case num < 99:
-		// TODO: RarityフィールドがRaritySRで
-		// Nameフィールドが"ドラゴン"のCard型のポインタを返す
-		return &Card{Rarity: RaritySR, Name: "ドラゴン2"}
+		return &Card{Rarity: RaritySR, Name: "ドラゴン"}
 	default:
 		return &Card{Rarity: RarityXR, Name: "イフリート"}
 	}
--- main.go
+++ main.go
@@ -1,25 +1,99 @@
-// STEP01: ファイルを分けよう
+// STEP04: エラーとパニックの違いを知ろう
 
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 
 	"github.com/mochi22/gacha_go_test/gacha"
 )
 
-func main() {
-	p := gacha.NewPlayer(10, 100)
+var (
+	flagCoin    int
+	flagResults string
+	flagSummary string
+)
 
-	n := inputN(*p)
-	results, summary := gacha.DrawN(p, n)
-	saveResults(results)
-	saveSummary(summary)
+var (
+	regexpResults = regexp.MustCompile(`^results.*\.txt$`)
+	// TODO: `^summary.*\.txt$`を正規表現としてコンパイルし変数regexpSummaryに代入
+	regexpSummary = regexp.MustCompile(`^summary.*\.txt$`)
+)
+
+func init() {
+	flag.IntVar(&flagCoin, "coin", 0, "コインの初期枚数")
+	flag.StringVar(&flagResults, "results", "results.txt", "結果ファイルの名前")
+	flag.StringVar(&flagSummary, "summary", "summary.txt", "集計ファイルの名前")
 }
 
-// TODO: 引数の型をgacha.Playerのポインタにする
-func inputN(p gacha.Player) int {
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	flag.Parse()
+
+	if !regexpResults.MatchString(flagResults) {
+		return fmt.Errorf("結果ファイル名が不正(%s)", flagResults)
+	}
+
+	if !regexpSummary.MatchString(flagSummary) {
+		return fmt.Errorf("集計ファイル名が不正(%s)", flagSummary)
+	}
+
+	tickets, err := initialTickets()
+	if err != nil {
+		return err
+	}
+	p := gacha.NewPlayer(tickets, flagCoin)
+	play := gacha.NewPlay(p)
+
+	n := inputN(p)
+	for play.Draw() {
+		if n <= 0 {
+			break
+		}
+		fmt.Println(play.Result())
+		n--
+	}
+
+	if err := play.Err(); err != nil {
+		return fmt.Errorf("ガチャを%d回引く:%w", n, err)
+	}
+
+	if err := saveResults(play.Results()); err != nil {
+		return err
+	}
+
+	if err := saveSummary(play.Summary()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func initialTickets() (int, error) {
+	if flag.NArg() == 0 {
+		return 0, errors.New("ガチャチケットの枚数を入力してください")
+	}
+
+	num, err := strconv.Atoi(flag.Arg(0))
+	if err != nil {
+		return 0, fmt.Errorf("ガチャチケット数のパース(%q):%w", flag.Arg(0), err)
+	}
+
+	return num, nil
+}
+
+func inputN(p *gacha.Player) int {
 
 	max := p.DrawableNum()
 	fmt.Printf("ガチャを引く回数を入力してください（最大:%d回）\n", max)
@@ -37,44 +111,40 @@ func inputN(p gacha.Player) int {
 	return n
 }
 
-func saveResults(results []*gacha.Card) {
-	f, err := os.Create("results.txt")
+func saveResults(results []*gacha.Card) (rerr error) {
+	f, err := os.Create(flagResults)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return fmt.Errorf("%sの作成:%w", flagResults, err)
 	}
 
 	defer func() {
-		if err := f.Close(); err != nil {
-			//標準エラー出力にerrを出力する
-			fmt.Fprintln(os.Stderr, err)
+		if err := f.Close(); err != nil && rerr == nil {
+			rerr = fmt.Errorf("%sのクローズ:%w", flagResults, err)
 		}
 	}()
 
 	for _, result := range results {
-		_, err = fmt.Fprintln(f, result)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+		fmt.Fprintln(f, result)
 	}
+
+	return nil
 }
 
-func saveSummary(summary map[gacha.Rarity]int) {
-	f, err := os.Create("summary.txt")
+func saveSummary(summary map[gacha.Rarity]int) (rerr error) {
+	f, err := os.Create(flagSummary)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return fmt.Errorf("%sの作成:%w", flagSummary, err)
 	}
 
 	defer func() {
-		if err := f.Close(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
+		if err := f.Close(); err != nil && rerr == nil {
+			rerr = fmt.Errorf("%sのクローズ:%w", flagSummary, err)
 		}
-
 	}()
 
 	for rarity, count := range summary {
 		fmt.Fprintf(f, "%s %d\n", rarity.String(), count)
 	}
+
+	return nil
 }
